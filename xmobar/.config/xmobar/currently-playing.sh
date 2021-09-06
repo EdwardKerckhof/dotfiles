777fc7569d76249015d37c372f1b84fb3d47dff1
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-09-07 00:31:45 +0200

    add more dotfiles

---
+++ xmobar/.config/xmobar/currently-playing.sh
@@ -0,0 +1,6 @@
+running=$(pidof spotify)
+if [ "$running" != "" ]; then
+    artist=$(playerctl --player=spotify,chromium,vlc metadata artist)
+    song=$(playerctl --player=spotify,chromium,vlc metadata title | cut -c 1-60)
+    echo -n "$artist · $song"
+fi 
